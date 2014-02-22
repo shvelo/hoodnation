39ceb511f92e569c9281d6d5f91187191fa90d62
Nick Shvelidze <captain@pirrate.me>
2014-02-22 18:34:08 +0400

    IT works

---
+++ js/resources.js
@@ -0,0 +1,14 @@
+//game resources
+game.resources = [
+    /**
+     * Graphics.
+     */
+    // our level tileset
+    {name: "Tileset64x64",  type:"image", src: "art/tileset/Tileset64x64.png"},
+     
+    /* 
+     * Maps. 
+     */
+    {name: "toilet", type: "tmx", src: "art/map/toilet.tmx"}
+ 
+];
